33b428ae4c72b95a5aacd4b6aefe6f3d74f43637
Sowetnikov Viktor <sowetnikov2010@yandex.ru>
2020-12-08 13:49:40 +0400

    add: trash, inputURL field and styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gallery = document.querySelector(".gallery");
+
 let count = 0;
 
 document.addEventListener('drop',event=>event.preventDefault());
@@ -6,7 +7,7 @@ document.addEventListener('dragover',event=>event.preventDefault());
 
 gallery.addEventListener('drop',event => {
     event.preventDefault();
-    if(event.dataTransfer.files[0].type.startsWith("image")){
+    if(event.dataTransfer.files[0] && event.dataTransfer.files[0].type.startsWith("image")){
         const file = event.dataTransfer.files[0];
         const url = URL.createObjectURL(file);
         addToGallery(url);
@@ -14,12 +15,28 @@ gallery.addEventListener('drop',event => {
 })
 
 document.querySelector(".buttonLoad").addEventListener("click",()=>{
-    const file = document.querySelector(".filePath").files[0];
-    getDataFromJSON(file).then(data=>{
-        data.galleryImages.forEach(element => {
-            addToGallery(element.url);
-        });
-    });
+    if(document.querySelector(".filePath").value !==""){
+        const file = document.querySelector(".filePath").files[0];
+        if(file.type.endsWith("json")){        
+            getDataFromJSON(file).then(data=>{
+                data.galleryImages.forEach(element => {
+                    addToGallery(element.url);
+                });
+            });
+        }
+        else if(file.type.startsWith("image")){
+            const url = URL.createObjectURL(file);
+            addToGallery(url)
+        }
+
+    } else if(document.querySelector(".fileUrl").value !==""){
+        console.log(document.querySelector(".fileUrl").value)
+        const url = document.querySelector(".fileUrl").value;
+        addToGallery(url)
+    }
+    document.querySelector(".fileUrl").value = "";
+    document.querySelector(".filePath").value = "";
+
 })
 
 function addToGallery(url){
@@ -35,3 +52,24 @@ async function getDataFromJSON(file) {
     const data = await response.json();
     return data;
 }   
+
+document.addEventListener("dragstart",event=>{
+    console.log(event.target)   
+    event.dataTransfer.setData('id',event.target.id)
+})
+
+document.querySelector(".trash").addEventListener('drop',event => {
+    event.target.classList.remove("open");
+    const img = document.getElementById(event.dataTransfer.getData("id"));
+    gallery.removeChild(img);
+    console.log(img)
+})
+
+document.querySelector(".trash").addEventListener("dragover",event => {
+    event.target.classList.add("open");
+    console.log(event.target)
+})
+document.querySelector(".trash").addEventListener("dragleave",event => {
+    event.target.classList.remove("open");
+    console.log("leave")
+})
